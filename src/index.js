74eb91ff0c9761d2f97f8ebc2c50982f407eeb94
Henry Sullivan <sullivanbhenry@gmail.com>
2022-11-15 11:15:21 -0800

    converts temperature to farenheit
    Co-authored-by: Vera Weikel <vera.weikel@gmail.com>

--- src/index.js
+++ src/index.js
@@ -5,12 +5,14 @@ import './css/styles.css';
 
 function getWeather(city) {
   let request = new XMLHttpRequest();
-  const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`;
+  const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}&units=imperial`;
 
   request.addEventListener("loadend", function() {
     const response = JSON.parse(this.responseText);
     if (this.status === 200) {
       printElements(response, city);
+    } else {
+      printError(this, city);
     }
   });
 
@@ -20,9 +22,13 @@ function getWeather(city) {
 
 // UI Logic
 
+function printError(request, city) {
+  document.querySelector('#showResponse').innerText = `There was an error accessing the weather data for ${city}:  ${request.status} ${request.statusText}`;
+}
+
 function printElements(apiResponse, city) {
   document.querySelector('#showResponse').innerText = `The humidity in ${city} is ${apiResponse.main.humidity}%.
-  The temperature in Kelvins is ${apiResponse.main.temp} degrees.`;
+  The temperature in Farenheit is ${apiResponse.main.temp} degrees.`;
 }
 
 function handleFormSubmission(event) {
